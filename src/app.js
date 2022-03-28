a226c9600fcd93410d47c3dcf7f7b0ce53ac9d18
Daniel Kravec <daniel@kravec.com>
2022-03-27 21:58:40 -0400

    1.0 (47.2022.03.27)

--- src/app.js
+++ src/app.js
@@ -90,7 +90,8 @@ app.use(cors({
         'http://localhost:5500', 
         'https://interact.dkravec.repl.co',
         'http://localhost:3000',
-        'http://127.0.0.1:5500'
+        'http://127.0.0.1:5500',
+        'https://interact-analytics.novapro.net'
     ],
     credentials: true
 }));
