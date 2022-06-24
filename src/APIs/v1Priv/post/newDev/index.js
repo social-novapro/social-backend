dbcd8ab040bd1755988790f10943ec1982070af8
Daniel Kravec <daniel@kravec.com>
2022-06-24 08:27:31 -0400

    1.0 (64.2022.06.24)

--- src/APIs/v1Priv/post/newDev/index.js
+++ src/APIs/v1Priv/post/newDev/index.js
@@ -4,11 +4,13 @@ const developerToken = require('../../../../schemas/developer/developerToken');
 const interactUserSchema = require('../../../../schemas/interactUserSchema');
 const { checkRequestTokens } = require('../../../../utils/checkRequestTokens');
 
-router.post('/:userID', async (req, res) => {
+router.post('/', async (req, res) => {
     const tokenData = await checkRequestTokens(req);
     if (tokenData.authorized == false) return res.status(401).send(tokenData);
 
-    const { userID } = req.params;
+    const { userid } = req.headers;
+    const userID = userid;
+    // const { userID } = req.params;
 
     if (!userID) return res.status(400).send("No userID provided");
 
@@ -16,7 +18,7 @@ router.post('/:userID', async (req, res) => {
     if (!userData) return res.status(400).send("no user found");
 
     const tokenSearch = await developerToken.findOne({userID});
-    if (tokenSearch) return res.status(400).send("Token already exists for this user. ");
+    if (tokenSearch) return res.status(400).send("Token already exists for this user.");
 
     const newDevToken = await newDeveloperToken(userID);
     const newTokenData = await developerToken.findOne({ _id: newDevToken }); 
