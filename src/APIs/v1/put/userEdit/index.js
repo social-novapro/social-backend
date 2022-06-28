43c0535bc02f8d8843d2d32238d7ac9695c396cc
Daniel Kravec <daniel@kravec.com>
2022-06-27 21:58:49 -0400

    1.0 (56.2022.06.27)

--- src/APIs/v1/put/userEdit/index.js
+++ src/APIs/v1/put/userEdit/index.js
@@ -13,12 +13,12 @@ router.put('/', async (req, res) => {
     const userid = headers.userid
     
     const editableAttributes = [
-      "newUsername",
-      "newDisplayname",
-      "newDescription",
-      "newPronouns",
-	  "newStatus"
-      // more?
+        "newUsername",
+        "newDisplayname",
+        "newDescription",
+        "newPronouns",
+        "newStatus"
+        // more?
     ]
 
     const userIDCheck = await interactUserSchema.findOne({ _id: userid});
@@ -26,12 +26,12 @@ router.put('/', async (req, res) => {
 
     var foundHeader = false
     for(const editableAttribute of editableAttributes) {
-		if (headers[editableAttribute] || headers[editableAttribute.toLowerCase()]) foundHeader = true;
+        if (headers[editableAttribute] || headers[editableAttribute.toLowerCase()]) foundHeader = true;
         // if(!headers.contains(editableAttribute))
-		// return res.status(400).send(searchError("E010"));
+        // return res.status(400).send(searchError("E010"));
     }
 
-	if (!foundHeader) return res.status(400).send({"error" : "not found"});// searchError("E010")
+    if (!foundHeader) return res.status(400).send({"error" : "not found"});// searchError("E010")
 
     var lastEdited = 0;
     if (!userIDCheck.lastEdit) lastEdited = 0;
@@ -49,38 +49,38 @@ router.put('/', async (req, res) => {
     if (!minutes) timeuntil = `${seconds} seconds`;
     else timeuntil = `${minutes} minutes and ${seconds} seconds`;
 
-	var acceptedChange = {
-		accepted: false,
-		reason: {},
-		types: []
-	}
+    var acceptedChange = {
+        accepted: false,
+        reason: {},
+        types: []
+    }
 
-	if (headers.newusername) {
-		const checkedUser = await checkUsername(headers.newusername);
-		if (checkedUser.error) return res.status(400).send(checkedUser.error);
-	}
-	else {
-		acceptedChange = true;
-	};
+    if (headers.newusername) {
+        const checkedUser = await checkUsername(headers.newusername);
+        if (checkedUser.error) return res.status(400).send(checkedUser.error);
+    }
+    else {
+        acceptedChange = true;
+    };
 
-	if (!acceptedChange) res.status(400).send({error: "Not accepted change."})
+    if (!acceptedChange) res.status(400).send({error: "Not accepted change."})
 
     // if (timediff < 1800000) return res.status(400).send({"error" : `You must wait ${timeuntil} before changing again.`});//searchError("E004"))
 
     await interactUserSchema.findOneAndUpdate(
         { _id: userid }, 
         { 
-          displayName: headers.newdisplayName || userIDCheck.displayName,
-          username: headers.newusername || userIDCheck.username, 
-          pronouns: headers.newpronouns || userIDCheck.pronouns, 
-          description: headers.newdescription || userIDCheck.description,
-		  statusTitle: headers.newstatus || userIDCheck.statusTitle,
-          lastEdit: currenttime,
+            displayName: headers.newdisplayName || userIDCheck.displayName,
+            username: headers.newusername || userIDCheck.username, 
+            pronouns: headers.newpronouns || userIDCheck.pronouns, 
+            description: headers.newdescription || userIDCheck.description,
+            statusTitle: headers.newstatus || userIDCheck.statusTitle,
+            lastEdit: currenttime,
         }, 
         { upsert: true }
     );
 
-	const UserData = await interactUserSchema.findOne({ _id: userid });
+    const UserData = await interactUserSchema.findOne({ _id: userid });
     if (!UserData) return res.status(404).send(searchError("E004"));//searchError("D002"))
     else return res.status(200).send({"new" : userIDCheck, "before" : UserData});
 })
