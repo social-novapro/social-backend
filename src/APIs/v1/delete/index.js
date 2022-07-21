4bd6fb5d7516e0f25b210553767b593e0fb444e8
Daniel Kravec <daniel@kravec.com>
2022-07-21 15:55:37 -0400

    1.0 (78.2022.07.21)

--- src/APIs/v1/delete/index.js
+++ src/APIs/v1/delete/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const removePost = require('./removePost');
 const unfollowUser = require('./unfollowUser');
 const unsubUser = require('./unsubUser');
+const unlikePost = require('./unlikePost');
 
 router.use('/removePost', removePost);
 router.use('/unfollowUser', unfollowUser);
 router.use('/unsubUser', unsubUser);
+router.use('/unlikePost', unlikePost);
 
 module.exports = router;
