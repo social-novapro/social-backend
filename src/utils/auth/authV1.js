eda7fd9060aba0a2235c97d01bd349c1068ec4b0
Daniel Kravec <daniel@kravec.com>
2023-05-02 00:00:47 -0400

    1.0 (104.2023.05.01)

--- src/utils/auth/authV1.js
+++ src/utils/auth/authV1.js
@@ -2,7 +2,14 @@ const { checkDevTokens } = require("../checkDevTokens");
 const { checkRequestTokens } = require("../checkRequestTokens");
 
 async function authV1(req, res, next) {
-    if (req.originalUrl == '/v1/auth/userLogin') {
+    if (
+        req.originalUrl == '/v1/get/analyticTrend' || 
+        req.originalUrl == '/v1/get/analyticTrend/' || 
+        req.originalUrl == '/v1/auth/userLogin' || 
+        req.originalUrl == '/v1/auth/userLogin/' ||
+        req.originalUrl == '/v1Priv/post/newUser' ||
+        req.originalUrl == '/v1Priv/post/newUser/'
+    ) {
         const { devtoken, apptoken } = req.headers;
         const tokenData = await checkDevTokens(devtoken, apptoken);
         if (tokenData.authorized === false) return res.status(401).send(tokenData);
