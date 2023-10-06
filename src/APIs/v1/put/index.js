688da822d05ff0c240f42b5e27b5dc7e875daf62
Daniel Kravec <daniel@kravec.com>
2023-10-05 23:20:25 -0400

    forgot to remove some routes

--- src/APIs/v1/put/index.js
+++ src/APIs/v1/put/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
-const editPost = require('./editPost');
 const editUsername = require('./editUsername');
 const editDisplayname  = require('./editDisplayname');
 const userEdit = require('./userEdit');
 
-router.use('/editPost', editPost);
 router.use('/editUsername', editUsername);
 router.use('/editDisplayname', editDisplayname);
 router.use('/userEdit', userEdit);
