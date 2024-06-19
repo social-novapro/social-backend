07fe5b92680f58caa56c7f7260455ba84dee0c1a
Daniel Kravec <daniel@kravec.com>
2024-06-19 17:02:14 -0400

    verifies for environment tokens before running added trace warnings to dev run

--- runMode/changeMode.js
+++ runMode/changeMode.js
@@ -3,13 +3,12 @@ const configFile = require('../config.json')
 const possible = ["prod", "dev"]
 
 function changeProdMode(changeTo) {
-    if (!possible.includes(changeTo)) throw "Not Valid Environment"
+    if (!possible.includes(changeTo)) throw " Requested environment was not valid"
     console.log(`---\nSetting up ${changeTo} environment`)
     if (configFile.current == changeTo) return true;
     
     configFile.current = changeTo;
 
-    console.log(configFile)
     fs.writeFileSync('./config.json', JSON.stringify(configFile, null, 4), (err) => {
         console.log(err)
     })
