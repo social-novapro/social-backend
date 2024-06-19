7baf9df1af82ad53cd7b5d92216dcd3cd25a509f
Daniel Kravec <daniel@kravec.com>
2024-06-19 15:21:03 -0400

    user getting now supports either username or userID in same route moved get user routes

--- src/APIs/v1/users/index.js
+++ src/APIs/v1/users/index.js
@@ -5,6 +5,7 @@ const reqDelete = require('./reqDelete');
 const edit = require('./edit');
 const privacy = require('./privacy');
 const badges = require('./badges');
+const getApi = require('./get');
 
 router.use('/profile', profile);
 router.use('/public', public);
@@ -12,5 +13,6 @@ router.use('/reqDelete', reqDelete);
 router.use('/edit', edit);
 router.use('/privacy', privacy);
 router.use('/badges', badges);
+router.use('/get', getApi);
 
 module.exports = router;
---
+++ src/utils/user/getUser/index.js
@@ -0,0 +1,80 @@
+const interactUserSchema = require("../../../schemas/interactUserSchema");
+const { getUserMentions } = require("../../post/tags");
+const { getUserPosts } = require("../../post/user");
+const { searchErrorV2 } = require("../../searchError");
+const { getUserBadges } = require("../badges");
+const { getUserPins } = require("../edit");
+
+async function getUser({userID, searchTerm}) {
+    const foundViaUsername = await interactUserSchema.findOne({usernameLc: searchTerm.toLowerCase()});
+    if (foundViaUsername) return foundViaUsername;
+
+    const foundViaUserID = await interactUserSchema.findOne({_id: searchTerm});
+    if (foundViaUserID) return foundViaUserID;
+
+    return searchErrorV2("C009", { userID });
+}
+
+async function getAllUserData({userID, searchTerm}) {
+    if (!userID) return searchErrorV2("B009", { userID })
+    if (!searchTerm) return searchErrorV2("C021", { userID })
+
+    const ownUser = await interactUserSchema.findOne({_id: userID});
+    if (!ownUser) return searchErrorV2("C020", { userID })
+
+    // user
+    const userData = await getUser({userID, searchTerm});
+    if (userData.error) return userData;
+
+    // posts
+    const postData = await getUserPosts({ 
+        userID: userData._id, 
+        requesterID: userID, 
+        coposts: true 
+    });
+
+    // badges
+    const badgeData = await getUserBadges({ userID: userData._id});
+
+    // pins
+    const pinData = await getUserPins({ ownUser, userData });
+
+    // mentions
+    const mentionData = await getUserMentions({userID: userData._id})
+
+    // TODO - likes (requires update)
+
+    const sendBack = {
+        included: {
+            user: "true",
+            posts: `${postData ? true : false}`,
+            pins: pinData.length > 0 ? true : false,
+            badges: badgeData.length > 0 ? true : false,
+            mentions: mentionData.length > 0 ? true : false
+        },
+        userData: userData,
+        postData: postData,
+        pinData: pinData,
+        badgeData: badgeData,
+        mentionData: mentionData
+    }
+
+    return sendBack;
+}
+
+async function getBasicUserData({userID, searchTerm}) {
+    if (!userID) return searchErrorV2("B009", { userID })
+    if (!searchTerm) return searchErrorV2("C021", { userID })
+
+    // user
+    const userData = await getUser({userID, searchTerm});
+    if (userData.error) return userData;
+
+    return userData;
+    
+}
+
+module.exports = {
+    getAllUserData, 
+    getBasicUserData
+}
