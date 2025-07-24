beec1d25867e70b89cfa2b8d6d5d3170449e86cf
dkravec <daniel@kravec.com>
2025-07-23 20:18:33 -0400

    Created a explore page for search, show newest 5 hashtags, users, and posts

--- src/APIs/v1/search/v2/index.js
+++ src/APIs/v1/search/v2/index.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
-const { searchV2 } = require('../../../../utils/search/searchV2');
+const { searchV2, explorePage } = require('../../../../utils/search/searchV2');
+
+router.get('/explore', async (req, res) => {
+    const found = await explorePage({
+        userID: req.headers.userid ?? null
+    });
+
+    if (!found || found.error) return res.status(400).send(found);
+    return res.status(200).send(found);
+});
 
 router.get('/', async (req, res) => {
     console.log("Searching for " + req.headers.lookupkey);
